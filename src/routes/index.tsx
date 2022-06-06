9a9d8fdbc7d8ef181c9245298183596aa38048fb
Fabio Goncalves <fgoncalves@cclindsa.com>
2022-06-06 16:22:23 -0300

    Aula 03

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route path="/pagina-inicial" element={<p>Página inicial...</p>} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
