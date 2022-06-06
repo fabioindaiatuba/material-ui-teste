55e8603e7f40bb18b50948978c5dc1ec0549e18a
Fabio Goncalves <fgoncalves@cclindsa.com>
2022-06-06 17:12:31 -0300

    Aula 05 - Configurado theme customizado do material

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button>Teste</Button>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="secondary">
+            Teste
+          </Button>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
