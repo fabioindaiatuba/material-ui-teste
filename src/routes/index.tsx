e432289c0067d6e844a4d1f237503e90ab8bb780
Fabio Goncalves <fgoncalves@cclindsa.com>
2022-06-06 15:42:37 -0300

    Aula 02

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
