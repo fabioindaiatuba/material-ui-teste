8962a898351ddb383b21a08ba7971758d936c451
Fabio Goncalves <fgoncalves@cclindsa.com>
2022-06-07 08:43:32 -0300

    Aula 08 - Menu Drawer

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,4 +1,14 @@
-import { Drawer, useTheme } from "@mui/material";
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
 import { Box } from "@mui/system";
 
 interface IMenuLateralProps {
@@ -10,7 +20,41 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
   return (
     <>
       <Drawer variant="permanent">
-        <Box width={theme.spacing(28)}>Teste</Box>
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              alt="Avatar teste"
+              sx={{
+                height: theme.spacing(12),
+                width: theme.spacing(12),
+              }}
+              src="https://ecdan.org/wp-content/themes/ecdan-child/assets/images/avatars/user-avatar.png"
+            />
+          </Box>
+
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
       </Drawer>
       <Box height="100vh" marginLeft={theme.spacing(28)}>
         {children}
